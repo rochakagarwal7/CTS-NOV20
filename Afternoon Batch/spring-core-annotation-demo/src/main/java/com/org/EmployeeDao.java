3245bf89cadd8a5f01644df30e408c03850c9171
Kishor <kishor1124@gmail.com>
2020-11-25 13:35:20 +0530

    spring annotation

---
+++ Afternoon Batch/spring-core-annotation-demo/src/main/java/com/org/EmployeeDao.java
@@ -0,0 +1,10 @@
+package com.org;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDao {
+	public void store() {
+		System.out.println("store() method in DAO layer");
+	}
+}
