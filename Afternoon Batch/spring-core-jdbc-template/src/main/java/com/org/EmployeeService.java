0888775ab2c7a8996e01e63b7e1bc26cea97c4db
Kishor <kishor1124@gmail.com>
2020-11-26 13:09:02 +0530

    added jdbctemplate code

--- Afternoon Batch/spring-core-jdbc-template/src/main/java/com/org/EmployeeService.java
+++ Afternoon Batch/spring-core-jdbc-template/src/main/java/com/org/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.org;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,14 @@ public class EmployeeService {
 
 	public void storeEmployee(Employee emp) {
 		dao.store(emp);
+	}
+	
+	public Employee fetchEmployeeById(int id) {
+		return dao.findEmployeeById(id);
+	}
+	
+	public List<Employee> fetchAllEmployees() {
+		return dao.findEmployees();
 	}
 
 }
--- Afternoon Batch/spring-core-jdbc-template/src/main/java/com/org/Main.java
+++ Afternoon Batch/spring-core-jdbc-template/src/main/java/com/org/Main.java
@@ -1,5 +1,7 @@
 package com.org;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,10 +10,16 @@ public class Main {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		
 		EmployeeService service = (EmployeeService) context.getBean("employeeService");
-		Employee emp = new Employee();
-		emp.setId(101);
-		emp.setName("Bruce");
-		emp.setSalary(55000);
-		service.storeEmployee(emp);
+//		Employee emp = new Employee();
+//		emp.setId(106);
+//		emp.setName("Frank");
+//		emp.setSalary(83000);
+//		service.storeEmployee(emp);
+		
+//		Employee emp = service.fetchEmployeeById(101);
+//		System.out.println("Id = "+emp.getId()+", Name = "+emp.getName()+", Salary = "+emp.getSalary());
+		
+		List<Employee> list = service.fetchAllEmployees();
+		list.forEach(e -> System.out.println(e));
 	}
 }
